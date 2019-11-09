020d9902899cecb0db5dfebc9620357543551b38
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-09 17:21:58 +0300

    Update Message.java

--- Message.java
+++ Message.java
@@ -15,4 +15,19 @@ public interface Message {
         millis -= TimeUnit.SECONDS.toMillis(seconds);
         return new long[]{minutes, seconds, millis};
     }
+
+    default long getMilliseconds(long[] arr) {
+        long ms = arr[2];
+        ms += arr[0] * 1000 * 60;
+        ms += arr[1] * 1000;
+        return ms;
+    }
+
+    default long[] convertMillis(long millis) {
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        millis -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+        millis -= TimeUnit.SECONDS.toMillis(seconds);
+        return new long[]{minutes, seconds, millis};
+    }
 }
