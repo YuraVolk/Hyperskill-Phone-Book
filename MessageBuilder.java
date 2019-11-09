d8dc3205f8e8d15553539508e4ccf303d76d7c3b
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-09 17:07:17 +0300

    Create MessageBuilder.java

---
+++ MessageBuilder.java
@@ -0,0 +1,25 @@
+package phonebook;
+
+public class MessageBuilder {
+    int requests;
+
+    MessageBuilder(int requests) {
+        this.requests = requests;
+    }
+
+    void produceMessage(int found, long start) {
+        Message message = new NoSortMessage(requests);
+        message.setFound(found);
+        message.setSearchTime(start);
+        message.print();
+    }
+
+    void produceMessage(int found, long start, long sortStart, int differ) {
+        SortMessage message = new SortMessage(requests);
+        message.setFound(found);
+        message.setSearchTime(start);
+        message.setSortTime(sortStart);
+        message.setDiffer(differ);
+        message.print();
+    }
+}
