f843d4abc73ab2b58d247549ef5cd8a8ddd3f848
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-09 17:07:33 +0300

    Create NoSortMessage.java

---
+++ NoSortMessage.java
@@ -0,0 +1,28 @@
+package phonebook;
+
+public class NoSortMessage implements Message {
+    long[] searchTime;
+    int found;
+    int size;
+
+    NoSortMessage(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void print() {
+        System.out.printf("Found %s / %s entries. Time taken: %s min. %s sec. %s ms.\n\n",
+                found, size,
+                searchTime[0], searchTime[1], searchTime[2]);
+    }
+
+    @Override
+    public void setSearchTime(long start) {
+        searchTime = getExecutionTime(start);
+    }
+
+    @Override
+    public void setFound(int found) {
+        this.found = found;
+    }
+}
